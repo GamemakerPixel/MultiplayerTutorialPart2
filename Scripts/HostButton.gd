b1821cfe872221caf2d3eb8a6df834cee3f40b54
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2020-05-31 15:49:37 -0400

    Update HostButton.gd

--- Scripts/HostButton.gd
+++ Scripts/HostButton.gd
@@ -3,7 +3,10 @@ extends Button
 signal set_connect_type
 
 func _ready():
-	$IP.text = "IP: " + str(IP.get_local_addresses()[7])
+	if OS.get_name() == "OSX":
+		$IP.text = "IP: " + str(IP.get_local_addresses()[7]) + " or " + (IP.get_local_addresses()[21])
+	else:
+		$IP.text = "IP: " + str(IP.get_local_addresses()[7])
 
 func host():
 	Net.initialize_server()
